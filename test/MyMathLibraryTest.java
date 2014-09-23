ffb12e0ba75cb1bd06560a7a08fa73e0d008ae7e
Marc Nazarian <marc.nazarian@gmail.com>
2014-09-23 23:32:46 +0200

    Add some tests with negative numbers

--- test/MyMathLibraryTest.java
+++ test/MyMathLibraryTest.java
@@ -5,4 +5,14 @@ public MyMathLibraryTest {
     Assert.Equals(3, MyMathLibrary.add(1, 2);
   }
 
+  @Test
+  public void ShouldAddTwoNegativeNumbers() {
+    Assert.Equals(-12, MyMathLibrary.add(-3, -9);
+  }
+
+  @Test
+  public void ShouldAddAPositiveNumberAndANegativeNumber() {
+    Assert.Equals(-1, MyMathLibrary.add(-5, 4);
+  }
+
 }
